527d9bdca69d964f7690edf71a846cb3bb9cdd29
Minwei Xu <faceswilliam@gmail.com>
2016-01-05 16:17:30 -0800

    id fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var App = require('./components/AppManager.jsx');
 
-ReactDOM.render(<div> <App/> </div>, document.getElementById('weatherApp'));
+ReactDOM.render(<div> <App/> </div>, document.getElementById('calculator'));
